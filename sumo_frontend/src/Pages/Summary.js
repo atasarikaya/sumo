b951e6bbefc6ad7a506d09553fcb65ea515bb790
CodedTheBear <inan.berk.business@gmail.com>
2021-12-20 13:58:19 +0300

    update

--- sumo_frontend/src/Pages/Summary.js
+++ sumo_frontend/src/Pages/Summary.js
@@ -73,7 +73,6 @@ function Summary() {
                         </div>
                         <h2>{profile.name}</h2>
                         <h4>Income: {profile.income ? profile.income + "₺" : "Unknown"}</h4>
-                        <h4>Subscription: {profile.subscription ? "Subscribed" : "Not Subscribed"}</h4>
                         <h4>Location: {profile.location ? profile.location : "Unknown"}</h4>
                     </div>
                 </div>
